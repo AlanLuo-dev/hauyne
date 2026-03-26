d8cebe7dee2dee72fed4081ca4acca72536b5c1c
Alan.Luo <35752648+AlanLuo-dev@users.noreply.github.com>
2026-03-26 14:06:05 +0800

    Fix：审计日志查询条件改用枚举对象接收 日志类型入参

--- hauyne-service-module/hauyne-service-audit/src/main/java/com/luoyx/hauyne/audit/query/AuditQuery.java
+++ hauyne-service-module/hauyne-service-audit/src/main/java/com/luoyx/hauyne/audit/query/AuditQuery.java
@@ -1,8 +1,8 @@
 package com.luoyx.hauyne.audit.query;
 
 import com.luoyx.hauyne.audit.enums.AuditEnum;
-import com.luoyx.hauyne.validation.constraint.EnumCheck;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 /**
@@ -17,12 +17,13 @@ public class AuditQuery {
      * 数据的主键ID
      */
     @Schema(description = "数据的主键ID")
+    @NotNull(message = "数据的主键ID 必填！")
     private Long localId;
 
     /**
      * 数据的日志类型名称
      */
     @Schema(description = "数据的日志类型名称")
-    @EnumCheck(enumClazz = AuditEnum.class, getterMethod = "getTypeName", message = "日志类型名称不正确")
-    private String typeName;
+    @NotNull(message = "日志类型名称不合法")
+    private AuditEnum auditEnum;
 }
