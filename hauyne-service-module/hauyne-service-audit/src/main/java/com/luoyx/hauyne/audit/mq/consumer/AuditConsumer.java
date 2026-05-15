a72d90ffb313cb95428a7ec63bf63d9cd9f2b055
Alan.Luo <35752648+AlanLuo-dev@users.noreply.github.com>
2026-05-15 14:14:34 +0800

    Style: 优化审计日志消息的日志打印

--- hauyne-service-module/hauyne-service-audit/src/main/java/com/luoyx/hauyne/audit/mq/consumer/AuditConsumer.java
+++ hauyne-service-module/hauyne-service-audit/src/main/java/com/luoyx/hauyne/audit/mq/consumer/AuditConsumer.java
@@ -4,6 +4,7 @@ import com.luoyx.hauyne.audit.api.constant.MQConstant;
 import com.luoyx.hauyne.audit.api.dto.JaversAuditMessage;
 import com.luoyx.hauyne.audit.api.enums.AuditTypeEnum;
 import com.luoyx.hauyne.audit.service.AuditService;
+import com.luoyx.hauyne.framework.utils.JsonUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -18,7 +19,7 @@ public class AuditConsumer {
 
     @RabbitListener(queues = MQConstant.AUDIT_QUEUE)
     public <U> void receive(JaversAuditMessage<U> auditMessage) {
-        log.info("AuditConsumer 接收到消息: {}", auditMessage);
+        log.info("AuditConsumer 接收到消息: {}", JsonUtil.toString(auditMessage));
 
         Long authorId = auditMessage.getAuthorId();
         U payload = auditMessage.getPayload();
