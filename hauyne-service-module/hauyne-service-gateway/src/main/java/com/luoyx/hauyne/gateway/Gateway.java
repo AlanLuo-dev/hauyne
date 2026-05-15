e1ed0540dde0ac18eac65bcfad1a4047b118aac4
Alan.Luo <35752648+AlanLuo-dev@users.noreply.github.com>
2026-05-15 17:45:00 +0800

    Feat: 集成SpringDoc到 网关

--- hauyne-service-module/hauyne-service-gateway/src/main/java/com/luoyx/hauyne/gateway/Gateway.java
+++ hauyne-service-module/hauyne-service-gateway/src/main/java/com/luoyx/hauyne/gateway/Gateway.java
@@ -9,6 +9,16 @@ import org.springframework.core.env.Environment;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties.SwaggerUrl;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import static org.springdoc.core.utils.Constants.DEFAULT_API_DOCS_URL;
+
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 /**
  * 网关主程序
  *
@@ -33,4 +43,20 @@ public class Gateway {
                 env.getProperty("server.port"));
     }
 
+    @Bean
+    @Lazy(false)
+    public Set<SwaggerUrl> apis(RouteDefinitionLocator locator,
+                                SwaggerUiConfigProperties swaggerUiConfigProperties) {
+        Set<SwaggerUrl> urls = new HashSet<>();
+        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+        definitions.stream()
+            .filter(RouteDefinition routeDefinition -> routeDefinition.getId().matches("service-.*"))
+            .forEach(RouteDefinition routeDefinition -> {
+                String name = routeDefinition.getId().replaceAll("service-", "");
+                SwaggerUrl swaggerUrl = new SwaggerUrl(name, name + DEFAULT_API_DOCS_URL, null);
+                urls.add(swaggerUrl);
+            });
+        swaggerUiConfigProperties.setUrls(urls);
+        return urls;
+    }
 }
