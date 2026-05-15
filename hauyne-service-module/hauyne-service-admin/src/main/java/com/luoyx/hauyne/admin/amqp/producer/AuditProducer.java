1a3ae61b9f10ad3a08546224db5a3af297e83b1d
Alan.Luo <35752648+AlanLuo-dev@users.noreply.github.com>
2026-05-15 14:27:08 +0800

    Fix：异步发送操作日志MQ消息

--- hauyne-service-module/hauyne-service-admin/src/main/java/com/luoyx/hauyne/admin/amqp/producer/AuditProducer.java
+++ hauyne-service-module/hauyne-service-admin/src/main/java/com/luoyx/hauyne/admin/amqp/producer/AuditProducer.java
@@ -3,12 +3,15 @@ package com.luoyx.hauyne.admin.amqp.producer;
 import com.luoyx.hauyne.audit.api.constant.MQConstant;
 import com.luoyx.hauyne.audit.api.dto.JaversAuditMessage;
 import com.luoyx.hauyne.audit.api.enums.AuditTypeEnum;
+import com.luoyx.hauyne.framework.utils.JsonUtil;
 import com.luoyx.hauyne.security.util.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.stereotype.Component;
-
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Slf4j
 @Component
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Component;
 public class AuditProducer {
 
     private final RabbitTemplate rabbitTemplate;
+    private final AsyncTaskExecutor taskExecutor;
 
     /**
      * 审计日志 以MQ消息形式发送到审计队列
@@ -27,8 +31,28 @@ public class AuditProducer {
         auditMessage.setAuthorId(SecurityUtils.getCurrentSysUserId());
         auditMessage.setAuditType(AuditTypeEnum.COMMIT);
         auditMessage.setPayload(auditDTO);
+        log.info("事务状态: {}", TransactionSynchronizationManager.isSynchronizationActive());
 
-        this.rabbitTemplate.convertAndSend(MQConstant.AUDIT_EXCHANGE, MQConstant.AUDIT_ROUTING, auditMessage);
+        // 核心: 注册事务同步器
+        TransactionSynchronizationManager.registerSynchronization(
+                new TransactionSynchronization() {
+                    @Override
+                    public void afterCommit() {
+                        // 异步提交
+                        taskExecutor.execute(() -> {
+                            try {
+                                log.info("监听到事务提交，开始发送操作日志: {}", JsonUtil.toString(auditMessage));
+                                rabbitTemplate.convertAndSend(
+                                    MQConstant.AUDIT_EXCHANGE, MQConstant.AUDIT_ROUTING, auditMessage
+                                );
+                            } catch (Exception e) {
+                                // 可扩展: 失败日志、本地消息表重试
+                                log.error("事务提交后异步发送MQ失败", e);
+                            }
+                        });
+                    }
+                }
+        );
     }
 
 
