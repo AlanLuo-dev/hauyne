556b43b3e2f303c928308cd6332c6c319d7f6162
罗英雄 <luo_yingxiong@163.com>
2026-09-17 16:27:27 +0800

    Fix: 刷新令牌过期时，增加「登录超时」提示返回给前端

--- hauyne-service-module/hauyne-service-uaa/src/main/java/com/luoyx/hauyne/uaa/config/CustomAuthExceptionHandler.java
+++ hauyne-service-module/hauyne-service-uaa/src/main/java/com/luoyx/hauyne/uaa/config/CustomAuthExceptionHandler.java
@@ -6,7 +6,11 @@ import com.luoyx.hauyne.framework.utils.JsonUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2ErrorCodes;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 import java.io.IOException;
@@ -14,18 +18,29 @@ import java.io.IOException;
 public class CustomAuthExceptionHandler implements AuthenticationFailureHandler {
 
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request,
+    public void onAuthenticationFailure(@NotNull HttpServletRequest request,
                                         HttpServletResponse response,
-                                        AuthenticationException exception)
+                                        @NotNull AuthenticationException exception)
             throws IOException, ServletException {
 
         response.setContentType("application/json;charset=UTF-8");
         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 
-        APIError<?> apiError = new APIError<>(
-                ErrorCodeEnum.USER_REQUEST_PARAMETER_ERROR,
-                exception.getMessage()
-        );
+        String errorTips;
+        if (exception instanceof OAuth2AuthenticationException oauth2Exception) {
+            String errorCode = oauth2Exception.getError().getErrorCode();
+            String grantType = request.getParameter("grant_type");
+
+            if (OAuth2ErrorCodes.INVALID_GRANT.equals(errorCode)
+                    && AuthorizationGrantType.REFRESH_TOKEN.getValue().equals(grantType)) {
+                errorTips = "登录超时";
+            } else {
+                errorTips = errorCode;
+            }
+        } else {
+            errorTips = exception.getMessage();
+        }
+        APIError<?> apiError = new APIError<>(ErrorCodeEnum.USER_LOGIN_HAS_EXPIRED, errorTips);
 
         response.getWriter().write(JsonUtil.toString(apiError));
     }
